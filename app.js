5d6a8de3225987c1b5d9fb4aec22d05940f23506
asrin andirin <asrnandrn@gmail.com>
2023-03-27 20:33:43 +0300

    auth-starter

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+// middleware
+app.use(express.static('public'));
+
+// view engine
+app.set('view engine', 'ejs');
+
+// database connection
+const dbURI = 'mongodb://localhost:27017/node-auth';
+mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
+  console.log('Db Connected')
+})
+
+// routes
+app.get('/', (req, res) => res.render('home'));
+app.get('/smoothies', (req, res) => res.render('smoothies'));
+
+const PORT = process.env.PORT || 3000
+app.listen(3000, () => {
+  console.log(`App is listening in port: ${PORT}`)
+})
